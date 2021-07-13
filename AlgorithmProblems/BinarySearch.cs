69380d02f398318b5b0fc47f0bc953659c95214d
unknown <sivaranjanib99@gmail.com>
2021-07-13 12:42:53 +0530

    [siva]UC2 Finding the word using BST

--- AlgorithmProblems/BinarySearch.cs
+++ AlgorithmProblems/BinarySearch.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace AlgorithmProblems
 {
-    class _
+    public class BinarySearch
     {
+        public static int BinarySearchWord(string[] arr,  string x)
+        {
+            int start = 0, end = arr.Length - 1;
+            while (start <= end)
+            {
+                int mid = start + (end - start) / 2;
+                int res = x.CompareTo(arr[mid]);
+                //check if x is present at mid
+                if (res == 0)
+                    return mid;
+                // if x greater, ignore left half
+                if (res > 0)
+                    start = mid + 1;
+                // if x is smaller , igore right half
+                else
+                    end = mid - 1;
+            }
+            return -1;
+        }
+
+       
     }
 }
+
--- AlgorithmProblems/Program.cs
+++ AlgorithmProblems/Program.cs
@@ -4,12 +4,16 @@ namespace AlgorithmProblems
 {
     class Program
     {
+        private static object arr;
+        private static string element;
+
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to Algorithm problems");
             Console.WriteLine("Choose an option:");
             Console.WriteLine("1) Permutation combination of Word");
-            Console.WriteLine("2) Exit");
+            Console.WriteLine("2) BinarySearchTree");
+            Console.WriteLine("3) Exit");
             Console.WriteLine("\r\nSelect  an option:");
             switch(Console.ReadLine())
             {
@@ -19,8 +23,32 @@ namespace AlgorithmProblems
                     int n = str.Length;
                     PermutationOfWord.Permute(str, 0, n - 1);
                     break;
-               
-                default:
+
+                case "2":
+                    Console.WriteLine("enter size of array:");
+                    int size = Convert.ToInt32(Console.ReadLine());
+                    string[] array = new string[size];
+                    Console.WriteLine("enter elements of array:");
+                    for (int i =0; i<size; i++)
+                    {
+                        array[i] = Console.ReadLine();
+                    }
+                    Console.WriteLine("enter elements  to find ");
+                    string searchElement = Console.ReadLine();
+                    int index = BinarySearch.BinarySearchWord(array, searchElement);
+                    if(index == 1)
+                    {
+                        Console.WriteLine("Element is not present");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Element is  present ");
+                    }
+                   
+                    break;
+
+                case "3":
+
                     Console.WriteLine("enter the right option");
                     break;
                    
